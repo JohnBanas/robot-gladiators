4a5bc55f50df2bcee69dec3d7dd748a0dcc623a6
JPB <jbanas9124@gmail.com>
2021-02-19 12:28:32 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -60,17 +60,45 @@ var fight = function(enemyName){
     }  
   } 
 };
-
-for(var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    var pickedEnemyName = enemyNames[i];
-    //enemyHealth = 50;
-    fight(pickedEnemyName);
-  } else {
-    window.alert("You have lost your robot in battle! Game over!")
+var startGame = function() {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+      var pickedEnemyName = enemyNames[i];
+      enemyHealth = 50;
+      fight(pickedEnemyName);
+    } else {
+      window.alert("You have lost your robot in battle! Game over!")
+    }
   }
-}
+  endGame();
+};
+
+//function to end entire game
+var endGame = function() {
+  if (playerHealth > 0) {
+    window.alert("Great job, you survived the game! You now have a score of " 
+    + playerMoney + ".");
+  } else {
+    window.alert("You've lost your robot in battle.");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+  if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  } else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+  
+
+
+//start game when the page loads
+startGame();
 
 //Game states
 
@@ -81,4 +109,21 @@ for(var i = 0; i < enemyNames.length; i++) {
 
   //call fight function with enemy robot
   //create fresh health for each new enemy robot
-  
+
+//Need to create a function for game logic (wrap in startGame())
+//If player is defeated or there are no more enemies, create endGame() function that:
+  //*alerts the player's stats
+  //*Asks the player if they want to play again
+  //*If true, call the startGame() to restart game
+
+//Need too create option after defeat/skip an enemy (with more enemies to fight) to:
+  //Ask the player if they want to "shop"
+  //If no, continue 
+  //If yes, call shop() function
+  //In shop function:
+    //*refill health option
+    //*upgrade attack option
+    //*or leave shop
+  //If upgrade/refill subtract money from player and add attack or health
+  //If leave, alert goodbye and end function
+  //any other invalid command will call shop() again
